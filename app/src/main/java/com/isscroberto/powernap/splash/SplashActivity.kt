ef342fe35ce422e43fefaddbd75d2e51dd54f8b2
isscroberto <issc.roberto@outlook.com>
2018-05-05 22:10:51 -0500

    Add animated title to splash screen

--- app/src/main/java/com/isscroberto/powernap/splash/SplashActivity.kt
+++ app/src/main/java/com/isscroberto/powernap/splash/SplashActivity.kt
@@ -1,18 +1,62 @@
 package com.isscroberto.powernap.splash
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.isscroberto.powernap.R
 import com.isscroberto.powernap.main.MainActivity
+import android.view.ViewGroup.LayoutParams.FILL_PARENT
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
+import android.view.animation.TranslateAnimation
+import android.widget.RelativeLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash.*
+
 
 class SplashActivity : AppCompatActivity() {
 
+    val ANIMATION_DURATION:Long = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
+        // Title animation configuration.
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            textTitle.scaleX = value
+            textTitle.scaleY = value
+        }
+        valueAnimator.interpolator = BounceInterpolator()
+        valueAnimator.duration = ANIMATION_DURATION
+
+        // Set animator listener.
         val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        valueAnimator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {}
+
+            override fun onAnimationEnd(p0: Animator?) {
+                // Navigate to main activity on navigation end.
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {}
+
+            override fun onAnimationStart(p0: Animator?) {}
+
+        })
+
+        // Start animation.
+        valueAnimator.start()
     }
 }
