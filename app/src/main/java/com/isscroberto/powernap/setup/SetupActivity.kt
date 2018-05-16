c16c3564e26b09f86baf466c5b27cc194141381e
isscroberto <issc.roberto@outlook.com>
2018-05-16 17:18:30 -0500

    Add description text and animation to SetupActivity layout

--- app/src/main/java/com/isscroberto/powernap/setup/SetupActivity.kt
+++ app/src/main/java/com/isscroberto/powernap/setup/SetupActivity.kt
@@ -2,6 +2,8 @@ package com.isscroberto.powernap.setup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.transition.TransitionManager
+import android.view.View
 import com.isscroberto.powernap.R
 import kotlinx.android.synthetic.main.activity_setup.*
 
@@ -14,5 +16,12 @@ class SetupActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        layout_power.setOnClickListener(View.OnClickListener {
+            TransitionManager.beginDelayedTransition(layout_content);
+            layout_refresh.visibility = View.GONE
+            layout_recharge.visibility = View.GONE
+            layout_coffee.visibility = View.GONE
+        })
     }
 }
