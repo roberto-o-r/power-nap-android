a83ecf0a2132e58737c5c15b3aa2712f95106cd8
Roberto Orozco <issc.roberto@outlook.com>
2018-07-25 16:19:07 -0500

    Add tests file for NapFragment

---
+++ app/src/androidTest/java/com/isscroberto/powernap/nap/NapFragmentTest.kt
@@ -0,0 +1,13 @@
+package com.isscroberto.powernap.nap
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class NapFragmentTest {
+
+    @Test
+    fun countdownFinishes_showSummaryUi() {
+        // TODO: write test for countdownfinishes.
+    }
+
+}
